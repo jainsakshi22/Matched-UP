0210034c35bd0988618836b9607cbd6796898784
jainsakshi22 <jainsakshi22@gmail.com>
2015-03-17 12:33:40 +0530

    Setting up the storyboard

---
+++ Matched-Up/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Matched-Up
+//
+//  Created by Sakshi Jain on 17/03/15.
+//  Copyright (c) 2015 Sakshi Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end

fec9759428285efb0539c5c79fe429c2b8450536
jainsakshi22 <jainsakshi22@gmail.com>
2015-03-19 00:55:16 +0530

    Saving user information

--- Matched-Up/LoginViewController.m
+++ Matched-Up/LoginViewController.m
@@ -63,11 +63,91 @@
         }
         else
         {
+            [self updateUserInformation];
             [self performSegueWithIdentifier:@"loginToTabBarSegue" sender:self];
         }
     }];
     
 }
+
+#pragma mark - Helper method
+
+-(void)updateUserInformation
+{
+    // Send request to facebook
+    FBRequest *request = [FBRequest requestForMe];
+    
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        //handle response
+      if (!error)
+      {
+        //Parse the data received
+          NSDictionary *userDictionary = (NSDictionary *)result;
+          NSMutableDictionary *userProfile = [[NSMutableDictionary alloc] initWithCapacity:8];
+          if (userDictionary[@"name"])
+          {
+              userProfile[@"name"] = userDictionary[@"name"];
+          }
+          if (userDictionary[@"first_name"])
+          {
+              userProfile[@"first_name"] = userDictionary[@"first_name"];
+          }
+          if (userDictionary[@"location"][@"name"])
+          {
+              userProfile[@"location"] = userDictionary[@"location"][@"name"];
+          }
+          if (userDictionary[@"gender"])
+          {
+              userProfile[@"gender"] = userDictionary[@"gender"];
+          }
+          if (userDictionary[@"birthday"])
+          {
+              userProfile[@"birthday"] = userDictionary[@"birthday"];
+          }
+          if (userDictionary[@"interested_in"])
+          {
+              userProfile[@"interested_in"] = userDictionary[@"interested_in"];
+          }
+          
+          [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+          [[PFUser currentUser] saveInBackground];
+      }
+        else
+        {
+            NSLog(@"Error in facebook request %@",error);
+        }
+          
+    }];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
